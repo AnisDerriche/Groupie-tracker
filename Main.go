224a24af3dab9f12c34b3382922a3010578e55cd
Ruben DURIEZ <ruben.duriez@ynov.com>
2025-02-07 09:35:49 +0100

    belek

--- Main.go
+++ Main.go
@@ -4,54 +4,114 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-type album struct {
-	Index interface{} "json:'index'"
+type artist struct {
+	ID    int          `json:"id"`
+	Name  string       `json:"name"`
+	Image template.URL `json:"image"`
+}
+
+type relation struct {
+	ID             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+	ArtistName     string
+	ArtistImage    template.URL
+}
+
+type ApiResponse struct {
+	Index []relation `json:"index"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	relations, err := getrelation()
+	if err != nil {
+		http.Error(w, "Erreur de récupération des données", http.StatusInternalServerError)
+		return
+	}
+
+	artist, err := getartist()
+	if err != nil {
+		http.Error(w, "Erreur de récupération des artistes", http.StatusInternalServerError)
+		return
+	}
+
+	filter := strings.ToLower(r.URL.Query().Get("filter"))
+	var filtered []relation
+
+	for _, artist := range artist {
+		if strings.Contains(strings.ToLower(artist.Name), filter) {
+			for _, relation := range relations {
+				if relation.ID == artist.ID {
+					relation.ArtistName = artist.Name
+					relation.ArtistImage = artist.Image
+					filtered = append(filtered, relation)
+				}
+			}
+		}
+	}
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		http.Error(w, "Erreur lors du chargement de la page", http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, filtered)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.Handle("../static/", http.StripPrefix("/static/", fs))
 	port := ":8080"
 	fmt.Println("Serveur démarré sur http://localhost" + port)
 	http.ListenAndServe(port, nil)
 }
 
-func getartists() {
-	url := "https://groupietrackers.herokuapp.com/api/relation" //lien de l'api
+func getartist() ([]artist, error) {
+	url := "https://groupietrackers.herokuapp.com/api/artists" //lien de l'api
 
 	// geter
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Erreur lors de la requête : %v\n", err)
-		return
+		return nil, err
 	}
 	defer response.Body.Close()
 
-	if response.StatusCode != http.StatusOK { // verifie si il n'y a pas d'erreur dans le lien
-		fmt.Printf("Code HTTP inattendu : %d\n", response.StatusCode)
-		return
-	}
-	println(response)
-
-	var data album
-	err = json.NewDecoder(response.Body).Decode(&data) // permet de lire le le json et de le décoder
-	if err != nil {                                    //erreur si il y a
-		fmt.Printf("Erreur lors du décodage JSON : %v\n", err)
-		return
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
 	}
 
-	fmt.Printf("Données reçues : %+v\n", data)
+	var artists []artist
+	err = json.Unmarshal(body, &artists)
+	if err != nil {
+		return nil, err
+	}
+
+	return artists, nil
+}
+
+func getrelation() ([]relation, error) {
+	url := "https://groupietrackers.herokuapp.com/api/relation"
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var apiResponse ApiResponse
+	err = json.Unmarshal(body, &apiResponse)
+	if err != nil {
+		return nil, err
+	}
+	return apiResponse.Index, nil
 }
